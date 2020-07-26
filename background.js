12804d90a37e9accf4b3423e830db35dec6fe244
Kevin Lu <kevinlu1248@gmail.com>
2020-07-26 14:41:45 -0700

    Made switching between modes significantly faster

--- background.js
+++ background.js
@@ -2,47 +2,52 @@
 
 // Special thanks to FA for the two icons
 
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({plugin_is_on: false});
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({ plugin_is_on: false });
 });
 
 chrome.browserAction.onClicked.addListener((tab) => {
-	chrome.storage.sync.get(['plugin_is_on'], function(data) {
-		// alert(data.plugin_is_on)
-		if (data.plugin_is_on === true) {
-			chrome.storage.sync.set({plugin_is_on: false});
-			chrome.browserAction.setIcon({path: {'16': 'icons/off.png'}});
-		} else {
-			chrome.storage.sync.set({plugin_is_on: true});
-			chrome.browserAction.setIcon({path: {'16': 'icons/on.png'}});
-		}
-	});
+    chrome.tabs.query({}, function (tabs) {
+        var message = { action: "updatePageMode" };
+        for (var i = 0; i < tabs.length; ++i) {
+            chrome.tabs.sendMessage(tabs[i].id, message);
+        }
+    });
+    chrome.storage.sync.get(["plugin_is_on"], function (data) {
+        // alert(data.plugin_is_on)
+        if (data.plugin_is_on === true) {
+            chrome.storage.sync.set({ plugin_is_on: false });
+            chrome.browserAction.setIcon({ path: { "16": "icons/off.png" } });
+        } else {
+            chrome.storage.sync.set({ plugin_is_on: true });
+            chrome.browserAction.setIcon({ path: { "16": "icons/on.png" } });
+        }
+    });
 });
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		console.log("sending request");
-		if (request.contentScriptQuery == 'annotateText') {
-			$.ajax({
-				method: "POST",
-				url: 'http://157.245.229.128:5001/',
-				data: JSON.stringify(request), 
-				contentType: "application/json; charset=UTF-8",
-				success: function(data, status, xhr) {
-					console.log(data);
-					console.log(xhr);
-					sendResponse([data, status, xhr]);
-				},
-				error: function(xhr, textStatus, errorThrown) {
-					console.log(xhr);
-					console.log(textStatus);
-					console.log(errorThrown);
-					sendResponse([data, status, xhr]);
-				}
-			}).done(function(data) {
-				console.log(data);
-			});
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log("sending request");
+    console.log(request);
+    if (request.contentScriptQuery == "annotateText") {
+        $.ajax({
+            method: "POST",
+            url: "https://researchy-api.herokuapp.com/",
+            data: request,
+            success: function (data, status, xhr) {
+                console.log(data);
+                console.log(xhr);
+                sendResponse([data, status, xhr]);
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log(xhr);
+                console.log(textStatus);
+                console.log(errorThrown);
+                sendResponse(["", status, xhr]);
+            },
+        }).done(function (data) {
+            console.log(data);
+        });
 
-			return true;
-		}
-	});
+        return true;
+    }
+});
--- content.js
+++ content.js
@@ -2,86 +2,182 @@
 
 $ = jQuery;
 
-const EXCLUDE = ['www.google.com']
-const INCLUDE = ['www.ncbi.nlm.nih.gov',
-'www.wikipedia.org', 'example.com']
+const EXCLUDE = ["www.google.com"];
+const INCLUDE = ["www.ncbi.nlm.nih.gov", "www.wikipedia.org", "example.com"];
+const READER_CSS = `
+<link href="https://fonts.googleapis.com/css2?family=Literata:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+<style>
+	html {
+		font-family: 'Literata', serif;
+		display: flex;
+		line-height: 1.75;
+		padding: 2rem;
+	}
+
+	body {
+		max-width: 800px;
+		margin: auto;
+		text-align: justify;
+	}
+
+	title {
+		text-align: left;
+	}
+
+	p, annotated, li {
+		font-size: 20px;
+	}
+
+	gpe, cardinal, percentage, org {
+		background-color: pink;
+	}
+
+	cancer, organ, cell, tissue, gene_or_gene_product, simple_chemical, immaterial_anatomical_entity {
+	            background-color: aquamarine;
+	        }
+
+	term {
+		font-weight: bold;
+	}
+</style>
+`;
 
 var originalDoc = "<html>" + $("html").html() + "</html>";
 var annotatedDoc = "";
+var annotationIsCurrentlyOn = false;
 
 $(document).ready(() => {
 
-	var setDocToAnnotated = (toAnnotated) => {
-		var newDocString = toAnnotated ? annotatedDoc : originalDoc;
-		console.log(newDocString)
-		var newDoc = document.open("text/html", "replace");
-		newDoc.write(newDocString);
-		newDoc.close();
-	}
+    const DOC_HTML = "<html>" + $("html").html() + "</html>";
 
-	var update = () => {
-		chrome.storage.sync.get(['plugin_is_on'], (data) => {
-			if (data.plugin_is_on) {
-				setDocToAnnotated(true);
-			}
-		});
-	}
-	
-	if (INCLUDE.includes(document.domain)) {
-		var doc_html = "<html>" + $("html").html() + "</html>";
-		console.log('sending message to background.js');
-		chrome.runtime.sendMessage(
-			{contentScriptQuery: "annotateText", text: doc_html, url: document.location},
-			annotated => {
-				console.log("responded");
-				console.log(annotated[0]);
-				if (annotated[1] == 'success') {
-					var obj = annotated[0];
-					var annotateDoc = obj["annotated_tree"];
-					var domparser = new DOMParser();
-					var annotatedDocDOM = domparser.parseFromString(annotateDoc, 'text/html');
-					annotatedDocDOM.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', 
+    $("html")
+        .prepend(`<iframe id="annotatedHTML" style="width: 100vw; height: 100vh; display: none; background-color: white;"></iframe>`);
+    const ANNOTATED_IFRAME = $("iframe#annotatedHTML")
+    ANNOTATED_IFRAME.contents().find("head").html(READER_CSS);
+    ANNOTATED_IFRAME.contents().find("body").html("Loading...");
+
+    var setDocToAnnotated = (toAnnotated) => {
+        ANNOTATED_IFRAME.css('display', toAnnotated ? 'inline-block' : 'none');
+    };
+
+    var updatePageMode = () => {
+        chrome.storage.sync.get(["plugin_is_on"], (data) => {
+            console.log("Updating: setting plugin to " + data.plugin_is_on);
+            setDocToAnnotated(data.plugin_is_on);
+        });
+    };
+
+    chrome.runtime.onMessage.addListener((message, callback) => {
+        switch (message.action) {
+            case "updatePageMode":
+                updatePageMode();
+                break;
+        }
+    });
+
+    console.log(document.domain);
+    
+    console.log("sending message to background.js");
+    chrome.runtime.sendMessage(
+        {
+            contentScriptQuery: "annotateText",
+            text: DOC_HTML,
+            url: document.location,
+        },
+        annotated => {
+            console.log("responded");
+            if (annotated[1] == "success") {
+                var obj = annotated[0];
+                var annotateDoc = obj["annotated_tree"];
+                var domparser = new DOMParser();
+                var annotatedDocDOM = domparser.parseFromString(
+                    annotateDoc,
+                    "text/html"
+                );
+      //           annotatedDocDOM
+      //               .getElementsByTagName("head")[0]
+      //               .insertAdjacentHTML(
+      //                   "beforeend",
+      //                   `
+						// <link href="https://fonts.googleapis.com/css2?family=Literata:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+						// <style>
+						// html {
+						// 	font-family: 'Literata', serif;
+						// 	display: flex;
+						// 	line-height: 1.75;
+						// 	padding: 2rem;
+						// }
+
+						// body {
+						// 	max-width: 800px;
+						// 	margin: auto;
+						// 	text-align: justify;
+						// }
+
+						// title {
+						// 	text-align: left;
+						// }
+
+						// p, annotated, li {
+						// 	font-size: 20px;
+						// }
+
+						// gpe, cardinal, percentage, org {
+						// 	background-color: pink;
+						// }
+
+						// cancer, organ, cell, tissue, gene_or_gene_product, simple_chemical, immaterial_anatomical_entity {
+						//             background-color: aquamarine;
+						//         }
+
+						// term {
+						// 	font-weight: bold;
+						// }
+						// </style>`
+      //               );
+                var reading_level = "";
+                if (30 < obj.reading_ease < 50) {
+                    reading_level = "College";
+                } else if (30 > obj.reading_ease) {
+                    reading_level = "Graduate";
+                } else {
+                    reading_level = `Grade ${Math.round(obj.grade_level)}`;
+                }
+                annotatedDocDOM
+                    .getElementsByTagName("body")[0]
+                    .insertAdjacentHTML(
+                        "beforeBegin",
+                        `
+						<div style="text-align: right;position: fixed;top: 1rem;right: 1rem;">
+						Word count: ${obj.word_count} (${Math.round(
+                            obj.word_count / 250
+                        )} minute read)<br>
+						${reading_level} level reading
+						</div>
 						`
-						<link href="https://fonts.googleapis.com/css2?family=Literata:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-						<style>
-						html {
-							font-family: 'Literata', serif;
-							display: flex;
-							line-height: 1.75;
-							padding: 2rem;
-						}
+                    );
 
-						body {
-							max-width: 800px;
-							margin: auto;
-							text-align: justify;
-						}
-
-						title {
-							text-align: left;
-						}
-
-						p, annotated, li {
-							font-size: 20px;
-						}
-
-						gpe, cardinal, percentage, org {
-							background-color: pink;
-						}
-
-						cancer, organ, cell, tissue, gene_or_gene_product, simple_chemical, immaterial_anatomical_entity {
-						            background-color: aquamarine;
-						        }
-
-						term {
-							font-weight: bold;
-						}
-						</style>`);
-					annotatedDoc = annotatedDocDOM.documentElement.outerHTML
-				}
-				update();
-			});
-	}
-
-	
+                $("iframe#annotatedHTML")
+                    .contents()
+                    .find("body")
+                    .html(
+                        `
+						<div style="text-align: right;position: fixed;top: 1rem;right: 1rem;">
+						Word count: ${obj.word_count} (${Math.round(
+                            obj.word_count / 250
+                        )} minute read)<br>
+						${reading_level} level reading
+						</div>
+						`
+                    );
+                console.log(
+                    annotatedDocDOM.getElementsByTagName("body")[0].innerHTML
+                );
+                $("iframe#annotatedHTML").contents().find('body').append(annotatedDocDOM.getElementsByTagName('body')[0].innerHTML);
+                annotatedDoc = annotatedDocDOM.documentElement.outerHTML;
+            }
+            updatePageMode();
+        }
+    );
+    // }
 });
