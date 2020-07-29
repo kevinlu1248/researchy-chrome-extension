0b6eb597748de248f48f472c22d3c5cfc76c4477
Kevin Lu <kevinlu1248@gmail.com>
2020-07-28 21:25:35 -0700

    made popup.html

--- background.js
+++ background.js
@@ -24,6 +24,21 @@ chrome.runtime.onInstalled.addListener(function () {
 //     });
 // });
 
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: { hostEquals: "developer.chrome.com" },
+					}),
+				],
+				actions: [new chrome.declarativeContent.ShowPageAction()],
+			},
+		]);
+	});
+});
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	console.log("sending request");
 	if (request.contentScriptQuery == "annotateText") {
